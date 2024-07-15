1a948dee9d4d936cc175bfec874101f7e63d57f6
Defenser666 <144529414+Defenser666@users.noreply.github.com>
2024-07-15 11:38:22 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Navbarx from './navbar'
+import Cardx from './cards'
+import Dropdownx from './dropdown'
+import Loadingx from './loading'
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
+  // 2. Wrap NextUIProvider at the root of your app
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider>
+      <Navbarx />
+      <Cardx />
+      <Dropdownx />
+      <Loadingx />
+    </NextUIProvider>
   );
 }
 
-export default App;
+export default App
